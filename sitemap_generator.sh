9251a84aec75cb9a7f8f7cfc79b455e314ac7e0f
Tue Topholm <tt@sugee.dk>
2016-07-24 02:00:18 +0200

    Added the excludefilelist

--- sitemap_generator.sh
+++ sitemap_generator.sh
@@ -5,9 +5,10 @@ ping=false
 output="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 tmp=/tmp/
 name=sitemap
+excludefilelist=xml,inc,old,save,txt,js,LCK,zip,ZIP,bmp,BMP,jpg,jpeg,JPG,GIF,PNG,png,gif,CSV,csv,css,class,jar,txt,ico,svg,SVG
 DATE=`date +%Y-%m-%d`
 
-while getopts ":p:sd:o:n:" opt; do
+while getopts ":p:sd:o:n:e:" opt; do
   case $opt in
     p)
       protocol=$OPTARG
@@ -24,6 +25,9 @@ while getopts ":p:sd:o:n:" opt; do
       ;;
     n)
       name=$OPTARG
+      ;;
+    e)
+      excludefilelist=$OPTARG
       ;;
     \?)
       echo "Invalid option: -$OPTARG" >&2
@@ -48,7 +52,7 @@ fi
 
 mkdir /tmp/$sitepath
 cd /tmp/$sitepath
-wget --spider --recursive --level=inf --no-verbose --output-file=linklist.txt -R xml,inc,old,save,txt,js,LCK,zip,ZIP,bmp,BMP,jpg,jpeg,JPG,GIF,PNG,png,gif,CSV,csv,css,class,jar,txt,ico,svg,SVG $protocol://$sitedomain/
+wget --spider --recursive --level=inf --no-verbose --output-file=linklist.txt -R $excludefilelist $protocol://$sitedomain/
 grep -i URL linklist.txt | awk -F 'URL:' '{print $2}' | awk '{$1=$1};1' | awk '{print $1}' | sort -u | sed '/^$/d' > sortedurls.txt
 header='<?xml version="1.0" encoding="UTF-8"?><urlset
       xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
@@ -68,7 +72,7 @@ while read p; do
 done < sortedurls.txt
 echo "</urlset>" >> $name.xml
 gzip -c -9 sitemap.xml > $name.xml.gz
-cp $name.xml $name.xml.gz $name.txt $output
+/bin/cp -rf $name.xml $name.xml.gz $name.txt $output
 rm -rf /tmp/$sitepath
 
 if [ "$ping" = true ]; then
